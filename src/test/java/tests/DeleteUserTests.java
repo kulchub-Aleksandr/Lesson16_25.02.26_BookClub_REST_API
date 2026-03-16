621214172fcc67753503a4eb30fc05e7b04f1d67
kulchub-Aleksandr <kulchub@gmail.com>
2026-03-16 20:53:37 +0500

    fix DeleteUserTests

--- src/test/java/tests/DeleteUserTests.java
+++ src/test/java/tests/DeleteUserTests.java
@@ -1,12 +1,9 @@
 package tests;
 
 import models.login.LoginBodyModel;
-import models.login.SuccessfulLoginResponseModel;
 import models.registration.RegistrationBodyModel;
 import models.registration.SuccessfulRegistrationResponseModel;
-import models.update.*;
-import net.datafaker.Faker;
-import net.datafaker.providers.base.Text;
+import models.update.NotProvidedAuthenticationCredentialsResponseModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,14 +11,12 @@ import org.junit.jupiter.api.Test;
 import static allure.CustomAllureListener.withCustomTemplate;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static net.datafaker.providers.base.Text.DIGITS;
-import static net.datafaker.providers.base.Text.EN_UPPERCASE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static specs.login.LoginSpec.loginRequestSpec;
 import static specs.login.LoginSpec.successfulLoginResponseSpec;
 import static specs.registration.RegistrationSpec.registrationRequestSpec;
 import static specs.registration.RegistrationSpec.successfulRegistrationResponseSpec;
-import static specs.update.UpdateSpec.*;
+import static specs.update.UpdateSpec.notProvidedAuthenticationCredentialsResponseSpec;
 
 public class DeleteUserTests extends TestBase {
     private final TestData testData = new TestData();
@@ -39,7 +34,6 @@ public class DeleteUserTests extends TestBase {
     public void successfulDeleteUserTest() {
         step("Регистрация нового пользователя", () -> {
             RegistrationBodyModel registrationData = new RegistrationBodyModel(username, password);
-
             SuccessfulRegistrationResponseModel registrationResponse = given(registrationRequestSpec)
                     .body(registrationData)
                     .when()
@@ -51,16 +45,10 @@ public class DeleteUserTests extends TestBase {
 
             assertThat(registrationResponse.id()).isGreaterThan(0);
             assertThat(registrationResponse.username()).isEqualTo(username);
-            assertThat(registrationResponse.firstName()).isEqualTo("");
-            assertThat(registrationResponse.lastName()).isEqualTo("");
-            assertThat(registrationResponse.email()).isEqualTo("");
 
-            String ipAddrRegexp = "^((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}"
-                    + "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$";
-            assertThat(registrationResponse.remoteAddr()).matches(ipAddrRegexp);
         });
 
-       LoginBodyModel loginData = new LoginBodyModel(username, password);
+        LoginBodyModel loginData = new LoginBodyModel(username, password);
 
         String accessToken = step("Авторизация и получение токена для удаления пользователя ", () ->
                 given(loginRequestSpec)
@@ -89,39 +77,35 @@ public class DeleteUserTests extends TestBase {
     @DisplayName("Тест на проверку удаления не авторизованного пользователя")
     public void notProvidedAuthenticationCredentialsDeleteUserNegativeTest() {
         step("Регистрация нового пользователя", () -> {
-                    RegistrationBodyModel registrationData = new RegistrationBodyModel(username, password);
+            RegistrationBodyModel registrationData = new RegistrationBodyModel(username, password);
 
-                    SuccessfulRegistrationResponseModel registrationResponse = given(registrationRequestSpec)
-                            .body(registrationData)
-                            .when()
-                            .post("/users/register/")
-                            .then()
-                            .spec(successfulRegistrationResponseSpec)
-                            .extract()
-                            .as(SuccessfulRegistrationResponseModel.class);
-
-                    assertThat(registrationResponse.id()).isGreaterThan(0);
-                    assertThat(registrationResponse.username()).isEqualTo(username);
-                    assertThat(registrationResponse.firstName()).isEqualTo("");
-                    assertThat(registrationResponse.lastName()).isEqualTo("");
-                    assertThat(registrationResponse.email()).isEqualTo("");
-
-                    String ipAddrRegexp = "^((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}"
-                            + "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$";
-                    assertThat(registrationResponse.remoteAddr()).matches(ipAddrRegexp);
-                });
-
-        step("Удаление пользователя без авторизации и проверка ответа (401)", () -> {
-            NotProvidedAuthenticationCredentialsResponseModel deleteResponse = given()
-                    .filter(withCustomTemplate())
-                    .log().all()
+            SuccessfulRegistrationResponseModel registrationResponse = given(registrationRequestSpec)
+                    .body(registrationData)
                     .when()
-                    .delete("/users/me/")
+                    .post("/users/register/")
                     .then()
-                    .spec(notProvidedAuthenticationCredentialsResponseSpec)
+                    .spec(successfulRegistrationResponseSpec)
                     .extract()
-                    .as(NotProvidedAuthenticationCredentialsResponseModel.class);
+                    .as(SuccessfulRegistrationResponseModel.class);
 
+            assertThat(registrationResponse.id()).isGreaterThan(0);
+            assertThat(registrationResponse.username()).isEqualTo(username);
+
+        });
+
+        NotProvidedAuthenticationCredentialsResponseModel deleteResponse =
+                step("Удаление пользователя без авторизации и проверка ответа (401)", () -> {
+                    return given()
+                            .filter(withCustomTemplate())
+                            .log().all()
+                            .when()
+                            .delete("/users/me/")
+                            .then()
+                            .spec(notProvidedAuthenticationCredentialsResponseSpec)
+                            .extract()
+                            .as(NotProvidedAuthenticationCredentialsResponseModel.class);
+                });
+        step("Проверка текста ошибки в ответе", () -> {
             String actualDetail = deleteResponse.detail();
             String expectedDetail = "Authentication credentials were not provided.";
             assertThat(actualDetail).isEqualTo(expectedDetail);

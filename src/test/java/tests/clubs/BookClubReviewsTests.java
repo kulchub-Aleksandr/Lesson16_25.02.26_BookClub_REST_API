d74912445ec1d5b8c50fd081671600995d7dfb3c
kulchub-Aleksandr <kulchub@gmail.com>
2026-03-29 07:54:34 +0500

    add test in BookClubReviewsTests

--- src/test/java/tests/clubs/BookClubReviewsTests.java
+++ src/test/java/tests/clubs/BookClubReviewsTests.java
@@ -194,6 +194,82 @@ public class BookClubReviewsTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Тест на вызов отзывов на книгу, с авторизованным пользователем, не членом клуба, с созданием клуба")
+    public void getReviewsBookClubWithAnAuthorizedUserNotMemberClubCreatingClubTest() {
+
+        SuccessfulRegistrationResponseModel registrationUserResponse
+                = api.users.registration(new RegistrationBodyModel(username, password));
+
+        step("Проверка соответствия отправленных данных с данными в ответе", () -> {
+            assertThat(registrationUserResponse.username()).isEqualTo(username);
+        });
+
+        String actualAccessToken = api.auth.loginAndGetAccessToken(new LoginBodyModel(username, password));
+
+        SuccessfulBookClubRegistrationResponseModel registrationResponseBookClub
+                = api.clubs.bookClubsRegistration(actualAccessToken, new SuccessfulBookClubRegistrationBodyModel(
+                bookTitle,
+                bookAuthors,
+                publicationYear,
+                description,
+                telegramChatLink));
+        step("Проверка соответствия полученных данных в ответе", () -> {
+            assertThat(registrationResponseBookClub.bookTitle()).isEqualTo(bookTitle);
+            assertThat(registrationResponseBookClub.bookAuthors()).isEqualTo(bookAuthors);
+        });
+
+        SuccessfulRegistrationResponseModel registrationUserResponse_1
+                = api.users.registration(new RegistrationBodyModel(username_1, password_1));
+
+        step("Проверка соответствия отправленных данных с данными в ответе", () -> {
+            assertThat(registrationUserResponse_1.username()).isEqualTo(username_1);
+        });
+
+        String actualAccessToken_1 = api.auth.loginAndGetAccessToken(new LoginBodyModel(username_1, password_1));
+
+//        api.clubs.bookClubMemberRegistration(actualAccessToken_1, registrationResponseBookClub.id());
+
+//        SuccessfulBookClubRegistrationResponseModel response
+//                = api.clubs.getClubById(actualAccessToken_1, registrationResponseBookClub.id());
+//
+//        step("Проверка что в члены клуба добавился второй пользователь", () -> {
+//            assertThat(response.members()).contains(response.owner());
+//            assertThat(response.members()).contains(registrationUserResponse_1.id());
+//        });
+
+        SuccessfulReviewsPostBookClubResponseModel reviewsResponse
+                = api.clubs.bookClubReviewsPost(actualAccessToken_1,
+                new SuccessfulReviewsPostBookClubBodyModel(
+                        registrationResponseBookClub.id(),
+                        newReview,
+                        5,
+                        22));
+
+        step("Проверка что отзыв второго пользователя добавился ", () -> {
+            assertThat(reviewsResponse.id()).isGreaterThan(0);
+            assertThat(reviewsResponse.club()).isGreaterThan(0);
+            assertThat(reviewsResponse.user().id()).isEqualTo(registrationUserResponse_1.id());
+            assertThat(reviewsResponse.user().username()).isEqualTo(registrationUserResponse_1.username());
+            assertThat(reviewsResponse.review()).isEqualTo(newReview);
+        });
+
+        SuccessfulReviewsGetBookClubResponseModel reviewsListResponse
+                = api.clubs.getReviewsBookClub(registrationResponseBookClub.id(), 1, 100);
+
+        step("Проверка что отзывы видны ", () -> {
+            assertThat(reviewsListResponse.count()).isGreaterThan(0);
+            assertThat(reviewsListResponse.results().getFirst().user().id()).isEqualTo(registrationUserResponse_1.id());
+            assertThat(reviewsListResponse.results().getFirst().user().username()).isEqualTo(registrationUserResponse_1.username());
+            assertThat(reviewsListResponse.results().getFirst().review()).isEqualTo(newReview);//
+        });
+
+        api.clubs.bookClubDelete(actualAccessToken, registrationResponseBookClub.id());
+        api.users.deleteUserAuthorized(actualAccessToken);
+        api.users.deleteUserAuthorized(actualAccessToken_1);
+
+    }
+
+    @Test
     @DisplayName("Тест на удаление отзыва на книгу, с авторизованным пользователем, с созданием клуба")
     public void deleteReviewsBookClubWithAnAuthorizedUserCreatingClubTest() {
 
@@ -235,6 +311,7 @@ public class BookClubReviewsTests extends TestBase {
 
         SuccessfulBookClubRegistrationResponseModel response =
                 step("Тест на получение информации клуба по ID и проверка что данные изменились", () -> {
+
                     return api.clubs.getClubById(actualAccessToken_1, registrationResponseBookClub.id());
                 });
 
